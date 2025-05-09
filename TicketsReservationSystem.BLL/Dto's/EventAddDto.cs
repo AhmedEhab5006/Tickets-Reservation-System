f6cccc8d2c1e5e4ae89ea90fecfaa37b79924e30
AhmedEhab5006 <ahm.ehab1510@gmail.com>
2025-05-09 23:48:13 +0300

    Vendor Fixed

--- TicketsReservationSystem.BLL/Dto's/EventAddDto.cs
+++ TicketsReservationSystem.BLL/Dto's/EventAddDto.cs
@@ -8,7 +8,7 @@ namespace TicketsReservationSystem.BLL.Dto_s
 {
     public class EventAddDto
     {
-        public string vendorId { get; set; }
+        public string? vendorId { get; set; }
         public string category { get; set; }
         public DateTime date { get; set; }
         public string location { get; set; }
